cbfcfa1ae6236236df6ef7e39f6fa231b4f387a0
Wezley Singleton <wezley.singleton@gmail.com>
2019-12-05 10:59:01 -0500

    added custom method stubs to repo

--- src/main/java/com/revature/coi/revanauts/repos/FeedbackRepository.java
+++ src/main/java/com/revature/coi/revanauts/repos/FeedbackRepository.java
@@ -1,5 +1,17 @@
 package com.revature.coi.revanauts.repos;
 
-public interface FeedbackRepository {
+import java.util.List;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.revature.coi.revanauts.models.Feedback;
+import com.revature.coi.revanauts.models.Skill;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, Long> {
+
+	List<Feedback> findFeedbackByAssociateId(Long associateId);
+	List<Feedback> findFeedbackBySkill(Skill skill);
+	
 }
