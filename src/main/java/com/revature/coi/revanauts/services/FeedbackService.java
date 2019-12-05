4b80a214262c579c6232e45cf88743f80b7ef59c
Wezley Singleton <wezley.singleton@gmail.com>
2019-12-05 10:25:03 -0500

    base code laid for service layer

--- src/main/java/com/revature/coi/revanauts/services/FeedbackService.java
+++ src/main/java/com/revature/coi/revanauts/services/FeedbackService.java
@@ -1,5 +1,16 @@
 package com.revature.coi.revanauts.services;
 
-public interface FeedbackService {
+import java.util.List;
 
+import com.revature.coi.revanauts.models.Feedback;
+
+public interface FeedbackService {
+	
+	List<Feedback> getAllFeedback();
+	List<Feedback> getAllFeedbackForAssociate(Feedback feedback);
+	List<Feedback> getAllFeedbackBySkill(Feedback feedback);
+	Feedback getFeedbackById(Feedback feedback);
+	boolean addNewFeedback(Feedback feedback);
+	void updateFeedback(Feedback feedback);
+	
 }
--- src/main/java/com/revature/coi/revanauts/services/FeedbackServiceImpl.java
+++ src/main/java/com/revature/coi/revanauts/services/FeedbackServiceImpl.java
@@ -1,5 +1,58 @@
 package com.revature.coi.revanauts.services;
 
-public class FeedbackServiceImpl {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.revature.coi.revanauts.models.Feedback;
+import com.revature.coi.revanauts.repos.FeedbackRepository;
+
+@Service
+public class FeedbackServiceImpl implements FeedbackService {
+
+	private FeedbackRepository feedbackRepo;
+	
+	@Autowired
+	public FeedbackServiceImpl(FeedbackRepository repo) {
+		this.feedbackRepo = repo;
+	}
+
+	@Override
+	public List<Feedback> getAllFeedback() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Feedback> getAllFeedbackForAssociate(Feedback feedback) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Feedback> getAllFeedbackBySkill(Feedback feedback) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Feedback getFeedbackById(Feedback feedback) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public boolean addNewFeedback(Feedback feedback) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void updateFeedback(Feedback feedback) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
