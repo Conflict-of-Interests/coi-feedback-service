621d7d23dfd935234cddb37ba3f497b76154080e
Wezley Singleton <wezley.singleton@gmail.com>
2019-12-05 10:58:14 -0500

    added exceptions

---
+++ src/main/java/com/revature/coi/revanauts/exceptions/ResourceCreationException.java
@@ -0,0 +1,11 @@
+package com.revature.coi.revanauts.exceptions;
+
+public class ResourceCreationException extends RuntimeException {
+	
+	private static final long serialVersionUID = 1L;
+
+	public ResourceCreationException(String message) {
+		super(message);
+	}
+
+}
---
+++ src/main/java/com/revature/coi/revanauts/exceptions/ResourceUpdateException.java
@@ -0,0 +1,11 @@
+package com.revature.coi.revanauts.exceptions;
+
+public class ResourceUpdateException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+
+	public ResourceUpdateException(String message) {
+		super(message);
+	}
+	
+}
